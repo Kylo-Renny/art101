283b74c179cbcf365d97fb320f15d923319339f8
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-10-21 13:55:16 -0700

    lab 4 finalization

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 - Pseudocoding and Problem Solving
+// Author: Ren Murphy
+// Date:10/20/23
 
-// Constants
-
-// Functions
+//Pseudocoding the Dinosaur Game
+//Generate a Desert Landscape
+  //Include Cacti in varying groups of 1 to 4
+  //Include Birds
+//Create Dinosaur Character
+  //Include Animation for Running, Jumping, Ducking, and Death
+//Implement Controls
+  //Space Button Starts Game and used for Jumping
+  //Down Arrow for Ducking
+//Create Variables
+  //Implement randomizer for Cacti and Bird obstacles
+  //Score begins at 0 and increases by distance Dinosaur goes
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
