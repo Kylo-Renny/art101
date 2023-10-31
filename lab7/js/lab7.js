cee7f040f4978d514831be2f0ad6758e7a64a30b
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-10-31 08:46:57 -0700

    fixing crap

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,23 +1,5 @@
-// index.js - Lab 6 - Objects and Arrays
+// index.js - Lab 7 - Functions
 // Author:Ren Murphy
-// Date:10/28/21
+// Date:11/3/21
 
 // Define Variables
-myTransport = ["Toyota Corolla Cross", "eBike", "Bus"];
-
-//create an object for my main ride
-myMainRide = {
-  make : "Toyota",
-  model : "Corolla Cross",
-  color : "Celestial Blue",
-  year : 2023,
-  age : function() {
-    return 2023 - this.year;
-  }
-}
-
-
-//output
-document.writeln("Kinds of transportation I use:", myTransport, "</br>");
-document.writeln("My Main Ride: <pre>", 
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
