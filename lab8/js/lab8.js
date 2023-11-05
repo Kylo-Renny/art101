89705d95eb722472501272a73f8af6603c9f3890
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-05 14:17:24 -0800

    lab8 commit

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -1,5 +1,22 @@
-// index.js - Lab 7 - Functions
+// index.js - Lab 8 - Anon Functions & Callbacks
 // Author:Ren Murphy
-// Date:11/3/21
+// Date:11/6/21
 
-// Define Variables
+// Console Experiments
+
+//my function that multiplies by 10
+function youMultiply(x){
+    return x * 10;
+}
+
+//my array numbesr
+array = [23,123,654,34,768]
+
+//the callback
+array.map(youMultiply);
+
+//the anon callback
+array.map(function(x) {
+    var results = x * 0.5
+    return results;
+})
