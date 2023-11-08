63f558dea5bb19655935bc64917b8a70cfc0fa7f
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-08 12:56:59 -0800

    hell

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -6,15 +6,19 @@
 function sortName() {
     var userName = window.prompt("Hey there! Can I get your name?");
     console.log("userName =",userName);
+
     //split string to array
     var nameArray = userName.split('');
     console.log("nameArray =",nameArray);
+
     //sort the array
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort=", nameArraySort);
+
     //join array back to a string
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
+    userName.toLower().split("").sort().join("");
     return nameSorted;
 }
 
