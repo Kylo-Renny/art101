0a490330d8abf909b8fd54c56486343182f25604
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-08 11:40:01 -0800

    lab4 resubmission

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -4,16 +4,35 @@
 
 //Pseudocoding the Dinosaur Game
 //Generate a Desert Landscape
-  //Include Cacti in varying groups of 1 to 4
-  //Include Birds
+  //Include Cacti in varying groups of 1 to 4 
+  //Include Birds 
 //Create Dinosaur Character
-  //Include Animation for Running, Jumping, Ducking, and Death
+  //Include Animation for Running, Jumping, Ducking, and Death 
 //Implement Controls
-  //Space Button Starts Game and used for Jumping
-  //Down Arrow for Ducking
+  //Main game loop:
+    //while the game is not over:
+    //Check for user input (e.g., spacebar to jump)
+    //Update dinosaur's position and animation:
+    //Apply gravity to the dinosaur's vertical position
+    //Keep the dinosaur above the ground:
 //Create Variables
-  //Implement randomizer for Cacti and Bird obstacles
-  //Score begins at 0 and increases by distance Dinosaur goes
+  //Initialize game variables:
+    //Dinosaur position (x, y)
+    // Ground height
+    //Game speed
+    //Score
+    //Obstacle list
+    //Implement randomizer for Cacti and Bird obstacles
+    //Score begins at 0 and increases by distance Dinosaur goes 
+    //Move obstacles from right to left       
+    //Check for collisions:
+      //Check if the dinosaur collides with any obstacle
+    //If there's a collision, end the game
+  //Game over:
+  //Display a game over message
+  //Show the final score
+    //Allow the player to restart the game if desired
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
