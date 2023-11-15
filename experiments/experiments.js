b29a804a1c6ac2f5bff6b28ba26157816e555af9
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-15 09:02:17 -0800

    hell yeah experiemnts

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -4,6 +4,12 @@
 //Creation Date: 11/15/23
 
 //Experiment 11/15/23
+console.log("Javascript is Working!");
+
+$("#my-button").click(function() {
+    console.log("Button Clicked!");
+});
+
 $("#my-button").click(
     function() {
 
