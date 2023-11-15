4e029ee07b9073421845d2443a60f145ca49fc3e
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-15 07:59:12 -0800

    saving

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -1,5 +1,5 @@
-// index.js - Lab 7 - Functions
+// index.js - Lab 10 - Javascript for the Web
 // Author:Ren Murphy
-// Date:11/3/21
+// Date:11/17/21
 
 // Define Variables
--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -1,5 +1,5 @@
-// index.js - Lab 7 - Functions
+// index.js - Lab 11 - JavaScript Events and Forms
 // Author:Ren Murphy
-// Date:11/3/21
+// Date:11/17/21
 
 // Define Variables
