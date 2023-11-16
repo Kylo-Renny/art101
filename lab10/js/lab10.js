c696daaaaecdc3d44947d98c8304703d238d6c66
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-16 08:10:55 -0800

    Lab 10 Working Javascript
    Functioning javascript  that generates random text- yay! Task X is working on the CSS to make the messages appear as texts

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -2,4 +2,32 @@
 // Author:Ren Murphy
 // Date:11/17/21
 
-// Define Variables
+// JQuery must be loaded for this script to work.
+
+function generateRandomText() {
+    ///Give the Text it Randomizes
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    ///The Smallest Message
+    const min = 3;
+    ///The Largest Message
+    const max = 100;
+
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+
+  
+// click listener for button
+$("#make-convo").click(function(){
+    ///get a new fake dialogue
+    const newText = generateRandomText();
+    ///append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+
+});
+
+
+
