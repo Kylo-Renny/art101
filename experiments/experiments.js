f3fecac7b1eac7ca181cce0a071f79f2489b3bef
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-11-17 08:51:45 -0800

    experiment 2

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -3,19 +3,42 @@
 //Author: Ren Murphy
 //Creation Date: 11/15/23
 
-//Experiment 11/15/23
+//Experiment 1 - Prompt 11/15/23
 console.log("Javascript is Working!");
 
 $("#my-button").click(function() {
     console.log("Button Clicked!");
 });
 
-$("#my-button").click(
-    function() {
+$("#my-button").click( function() {
 
     var name = prompt("Gimmie Your Name Please:");
 
     $('#title').html('Hello ' + name);
     });
+
 //////////////////////////////////////////////////////
- 
+//Experiment 2 - Data Input 11/17/23
+
+$('#my-second-button').click(function() {
+
+    var inputValue = $("#my-input").val();
+
+    console.log("Your input:", inputValue);
+
+    console.log("Button Clicked!");
+
+    var name =$("#my-input").val()
+
+    console.log("Prompt returned" + name);
+
+    $('#title2').html('Hello ' + name +"!");
+      
+    });
+
+//////////////////////////////////////////////////////
+//Experiment 3 - Anagram Attempt (Personal)    
+
+    
+      
+      
