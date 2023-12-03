73ee1750c32f2bde14218f5b38d77d9e3a741adc
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-12-03 10:39:51 -0800

    lab 15 set up

---
+++ lab15/js/lab15.js
@@ -0,0 +1,23 @@
+// index.js - Lab 15 - AJAX
+// Author:Ren Murphy
+// Date:12/4/21
+
+// First Test
+const ENDPOINT = "https://icanhazdadjoke.com/"
+// attach an event listener
+$("#go").click(function(){
+  // ajax this shit
+  $.ajax({
+    "url": ENDPOINT,
+    "dataType": "json",
+    "success": function(results){
+      console.log(results);
+ 
+      jokeText = results.joke;
+      // add to output div 
+      $("#output").append("<p>" + jokeText);
+    },
+      "error": function(){
+      }
+  })
+})
--- lab15/js/lab7.js
+++
@@ -1,5 +0,0 @@
-// index.js - Lab 7 - Functions
-// Author:Ren Murphy
-// Date:11/3/21
-
-// Define Variables
