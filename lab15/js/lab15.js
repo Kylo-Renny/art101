b788d0f889c18fc7b9009d5550bf7885a93d040b
Ren <146970873+Kylo-Renny@users.noreply.github.com>
2023-12-03 19:06:54 -0800

    Update lab15.js

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -21,3 +21,23 @@ $("#go").click(function(){
       }
   })
 })
+
+// Second Test
+//const ENDPOINT = "https://www.dnd5eapi.co/api/spells/"
+// attach an event listener
+//$("#go").click(function(){
+  // ajax this shit
+  //$.ajax({
+    //"url": ENDPOINT,
+    //"dataType": "json",
+   // "success": function(results){
+   //   console.log(results);
+ 
+    //  jokeText = results.joke;
+      // add to output div 
+    //  $("#output").append("<p>" + jokeText);
+   // },
+     // "error": function(){
+   //   }
+ // })
+//})
